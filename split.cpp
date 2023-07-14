a4def03f9afdd203abe06c7533a24c2b0f8c12b1
kmj0505 <menge0505@naver.com>
2023-07-14 11:30:08 +0900

    split test

---
+++ split.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split(string input, string delimiter){
+    vector<string> ret;
+    long long pos = 0;
+    string token = "";
+    
+    //input에서 delimeter 찾기(못 찾을 때까지 루프 반복)
+    while ((pos = input.find(delimiter))!= string::npos){
+        token = input.substr(0, pos);
+        ret.push_back(token);
+        input.erase(0, pos + delimiter.length());
+    }
+    ret.push_back(input);
+
+    return ret;
+}
+
+int main(){
+    string s = "Let's do the C++ practice!", d = " ";
+    vector<string> a = split(s,d);
+    for(string b:a) cout << b << "\n";
+}
